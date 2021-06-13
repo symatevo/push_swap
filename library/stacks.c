5a254df0d0ab4c97770dc3e64ffa1c9cd0aeb11e
Syuzanna Matevosyan <74954267+symatevo@users.noreply.github.com>
2021-06-14 00:31:36 +0400

    Add files via upload

---
+++ library/stacks.c
@@ -0,0 +1,59 @@
+#include "library.h"
+
+t_stack     *ft_new(int content)
+{
+	t_stack *rtn;
+
+	rtn = (t_stack *)malloc(sizeof(t_lst));
+	if (!rtn)
+		return (rtn = NULL);
+	rtn->chunk = 0;
+	rtn->next = NULL;
+	rtn->content = content;
+	return (rtn);
+}
+
+void	ft_lstadd_front(t_lst **alst, t_lst *new)
+{
+	if (!alst || !new)
+		return ;
+	new->next = *alst;
+	*alst = new;
+}
+
+int	ft_return_int(char *str, int len)
+{
+	int i;
+	int numb;
+
+	numb = 0;
+	i = 0;
+	len--;
+	while (len > 0)
+	{
+		if (*str == ' ')
+			len--;
+		str++;
+	}
+	numb = ft_atoi(str);
+	return (numb);
+}
+
+t_stack	*ft_create_stack(int len, char **str, int n)
+{
+	t_stack		*stack;
+    int         i;
+
+    stack = (n == 1) ? ft_new(ft_return_int(str[1], len)) : ft_new(ft_atoi(str[i]));
+    i = (n == 1) ? 0 : 1;
+	len = (n == 1) ? len-- : len;
+	while (len > 0)
+	{
+	    t_stack	*new;
+        new = (n == 1) ? ft_new(ft_return_int(str[1], len)) : ft_new(ft_atoi(str[i]));
+        ft_lstadd_front(&stack, new);
+        i = (n == 1) ? 0 : i + 1;
+        len--;
+	}
+	return (stack);
+}
