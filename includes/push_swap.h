b7d29e1d255385ba3fdf625a9cefe44cd57313ba
Syuzanna Matevosyan <74954267+symatevo@users.noreply.github.com>
2021-07-13 13:03:30 +0400

    Add files via upload

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,15 +1,8 @@
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
-
-#include "../libft/libft.h"
+# define PUSH_SWAP_H
 #include "../library/library.h"
 
-int     main(int argc, char **argv);
-void    error_doubles_check(t_stack *lst);
-void	sort_stack(t_stack **stack_a, t_stacks **stacks);
-void	sort_three(t_stacks **stack_a);
-void	sort_five(t_stacks **stack_a);
-void	main_sort(t_stacks **stacks, int size);
-int     short_way(t_stack *stack, int value);
-
+void	sort_three(int count, t_list **lst);
+void	sort_five(int count, t_list **a, t_list **b);
+void	sort_main(t_list **a, t_list **b, t_list **gc);
 #endif
